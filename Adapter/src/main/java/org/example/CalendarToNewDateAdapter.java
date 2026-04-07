fa5d5815e430fa90ddc9767c420ebbe20f2f4900
Osama-Aamer <osamaaamer2@gmail.com>
2026-04-07 23:36:18 +0300

    Builder and Adapter Assignments

---
+++ Adapter/src/main/java/org/example/CalendarToNewDateAdapter.java
@@ -0,0 +1,54 @@
+package org.example;
+
+import java.util.Calendar;
+
+public class CalendarToNewDateAdapter implements NewDateInterface {
+    private Calendar calendar;
+
+    public CalendarToNewDateAdapter() {
+        this.calendar = Calendar.getInstance();
+    }
+
+    @Override
+    public void setDay(int day) {
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+    }
+
+    @Override
+    public void setMonth(int month) {
+        // Calendar months are 0-indexed (0 = January, 11 = December)
+        // so we subtract 1 from the input
+        calendar.set(Calendar.MONTH, month - 1);
+    }
+
+    @Override
+    public void setYear(int year) {
+        calendar.set(Calendar.YEAR, year);
+    }
+
+    @Override
+    public int getDay() {
+        return calendar.get(Calendar.DAY_OF_MONTH);
+    }
+
+    @Override
+    public int getMonth() {
+        // Calendar months are 0-indexed, so we add 1 to return a 1-indexed month
+        return calendar.get(Calendar.MONTH) + 1;
+    }
+
+    @Override
+    public int getYear() {
+        return calendar.get(Calendar.YEAR);
+    }
+
+    @Override
+    public void advanceDays(int days) {
+        calendar.add(Calendar.DAY_OF_MONTH, days);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%02d/%02d/%04d", getDay(), getMonth(), getYear());
+    }
+}
