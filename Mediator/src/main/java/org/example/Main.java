ba732113c6d54d8a0f2dd1d299b14a39b6e577de
Osama-Aamer <osamaaamer2@gmail.com>
2026-04-22 12:14:24 +0300

    fix error for mediator Assignment

--- Mediator/src/main/java/org/example/Main.java
+++ Mediator/src/main/java/org/example/Main.java
@@ -3,14 +3,7 @@ package org.example;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-/**
- * Main class for the Chat Application using Mediator Pattern
- *
- * To run this application:
- * 1. Using Maven: mvn javafx:run
- * 2. Using IDE: Right-click and Run as Java Application
- * 3. Command line: java --module-path /path/to/javafx-sdk/lib --add-modules javafx.controls,javafx.fxml org.example.Main
- */
+
 public class Main extends Application {
     private ChatMediator chatRoom = new ChatRoom();
 
